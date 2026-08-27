750b77b9cbaf5aca8d38a83af787f1d9f4af842a
Many <ultimos23x@gmail.com>
2026-08-27 16:05:50 -0500

    array commit

---
+++ 01-Tipos/07-Arrays.js
@@ -0,0 +1,23 @@
+let lenguajes = [ 'python', 'ruby', 'JavaScript' ]
+
+console.log (lenguajes)
+
+// Nota: Los Arrays empiezan contando desde 0, entonces 'python es 0, ruby es 1 y JavaScript es 2, sin embargo, la longitud del array es de 3, ya que tiene 3 valores.
+
+//Si quiero que me imprima un valor especifico:
+
+// Python 
+
+console.log ( lenguajes [0])
+
+// Ruby
+
+console.log ( lenguajes [1])
+
+// JavaScript
+
+console.log ( lenguajes [2])
+
+// Si quiero añadir un valor mas al array, no tengo que buscarlo manualmente, puedo simplemente:
+
+lenguajes[3] = 'C'

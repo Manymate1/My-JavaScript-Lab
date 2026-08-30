2b81df5cdf7692375bd5ae3fc689421dfa1e9d99
Many <ultimos23x@gmail.com>
2026-08-30 11:04:35 -0500

    añadi archivo de asignacion a variables

---
+++ 02-Operadores/02-Asignacion.js
@@ -0,0 +1,13 @@
+let a = 5 // Quiero que el valor de esta variable sea 10
+
+a = a + 5 // se puede hacer de esta manera, redefiniendo la variable ya establecida en 5 y sumandole 5
+
+a += 5 // mejor hacerlo asi, se suma sin tener que redefinir la variable
+
+a -= 5
+a *= 5
+a /= 5
+a %= 5
+a **= 5
+
+console.log(a)

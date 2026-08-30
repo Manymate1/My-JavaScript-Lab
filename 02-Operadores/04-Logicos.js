e551578206b07f9f2fc89f21925950d172d423c6
Many <ultimos23x@gmail.com>
2026-08-30 15:48:20 -0500

    añadi archivo de operadores logicos

---
+++ 02-Operadores/04-Logicos.js
@@ -0,0 +1,19 @@
+// === - Operadores Logicos - ===
+
+let mayor = true
+let suscrito = true 
+
+// AND ( && )
+
+// console.log( true && true )
+// console.log( false && true )
+
+console.log ( mayor && suscrito ) // ¿Es "mayor" y "suscrito" verdaderos?
+
+// OR ( || )
+
+console.log ( mayor || suscrito ) // ¿cualquiera de los dos es verdadero?
+
+// NOT ( ! )
+
+console.log( !mayor ) // Cambia el valor de la variable a la contraria ( de true a false o de false a true )

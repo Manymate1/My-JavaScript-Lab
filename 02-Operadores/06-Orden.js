a67c621d454efee1cbdd41961cf66ea6d44aebfb
Many <ultimos23x@gmail.com>
2026-08-30 23:43:48 -0500

    Añadi archivo sobre Orden de operaciones

---
+++ 02-Operadores/06-Orden.js
@@ -0,0 +1,11 @@
+let resultado = 8 / 2 * ( 2 + 2 ) // NOTA: en el espacio entre 2 ( 2, de debe expresar con *, o sino saltara error
+
+console.log ( resultado )
+
+// Si quieres cambiar el resultado que dara, debes encerrar lo que queiras que cambie en el parentesis.
+
+let Resultado = 8 / ( 2 * (2 + 2) )
+
+console.log( Resultado )
+
+// La prioridad de parentesis, todo lo que este dentro de un parentesis se ejecutara primero, en este caso primero ( 2 + 2 ), posterior mente ( 2 * ) y finalmente 8.

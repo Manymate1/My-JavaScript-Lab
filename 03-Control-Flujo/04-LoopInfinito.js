be578fa4d861865baef78fcd943a6be0fa069f26
Many <ultimos23x@gmail.com>
2026-09-01 22:58:20 -0500

    añadi control de flujo Loop infinito

---
+++ 03-Control-Flujo/04-LoopInfinito.js
@@ -0,0 +1,12 @@
+let i = 0 
+
+while ( i < 10 ) {
+
+    console.log ( i )
+    // i++
+
+}
+
+// Mucho cuidado con los loops infinitos, estos podrian dañar gravemente el computador, se usan ( en ambientes moderados ), como para un carrusel de imagenes y se repite cada cierto tiempo.
+
+// !! Al ejecutar este codigo es posible que tu computadora se congele o deje de funcionar la pagina.

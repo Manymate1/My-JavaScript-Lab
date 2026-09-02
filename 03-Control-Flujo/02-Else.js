924fc83fbb8eb816752581bf7e2b919dd969626a
Many <ultimos23x@gmail.com>
2026-09-01 21:35:16 -0500

    Añadi control de flujo while

--- 03-Control-Flujo/02-Else.js
+++ 03-Control-Flujo/02-Else.js
@@ -1,10 +1,16 @@
-// Si por alguna razón la condicion de tu if no es cumplida, Else ejecutara la acción que se le fue dada en Lugar de la de If
+// Si por alguna razón la condicion de tu if no es cumplida, Else ejecutara la acción que se le fue dada en Lugar de la de If, si queremos mas de una instruccion, se usa Else If
 
-let edad = 15
+let edad = 15 // <-- Juega con esta variable a tu antojo
 
 if ( edad > 17 ){
 
     console.log (' Este Usuario Es Mayor De Edad ')
+
+}
+
+else if ( edad > 13 ){
+
+    console.log ( 'Usuario necesita supervisión de un adulto')
 
 }
 
@@ -16,5 +22,8 @@ else{
 
 
 // si " edad " es mayor a 17, imprime " Este usuario es Mayor de edad ", sino, imprime " Este Usuario Es Menor De Edad "
+
+// El codigo siempre evalua de arriba hacia abajo ( primero evalua si edad cumple si es mayor a 17, luego si es mayor a 13, y luego el ultimo else, y ejecutara el que primero cumpla la condición. )
+
 
 // !! Este Ejercicio No Es Compatible con la pseudoConsola, Usa la consola del navegador
---
+++ 03-Control-Flujo/03-While.js
@@ -0,0 +1,28 @@
+// Van a haber ocasiones en la que queremos que nuestro codigo se ejecute más de una vez.
+
+let i = 0 
+
+while ( i < 10 ) {
+
+    console.log ( i )
+    i++
+
+} // MIENTRAS i sea menor a 10, imprime i y luego de imprimirlo sumale 1.
+
+console.log (' Fuera del While ')
+
+/* ! NOTA: while repite en forma de bucle, cuando ejecutemos algo y no queremos que sea un bucle infinito debemos indicar algo que haga que se termine de cumplir la condicion dada para terminar con el bucle. 
+
+En este caso, el bucle no parara hasta que " i " Sea mayor a 10, para eso, cada vez que el bucle ejecute la accion dada, al final le sumaremos 1 a la variable, eventualmente sera mayor a 10 y el bucle parara, ya que:
+
+i = 11
+
+i < 10 ?
+
+False.
+
+* Para el bucle * 
+
+*/ 
+
+// !! Este Ejercicio No Es Compatible con la pseudoConsola, Usa la consola del navegador

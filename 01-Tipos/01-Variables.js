f478da2ab5dde6bd9d04d48dfca9af3d65dc7f6d
Many <ultimos23x@gmail.com>
2026-09-04 21:43:12 -0500

    version temprana de for of y comentarios para 01-variable

--- 01-Tipos/01-Variables.js
+++ 01-Tipos/01-Variables.js
@@ -1,3 +1,9 @@
+// Una variable es un espacio en la memoria RAM del computador que se usa para guardar información y asociarla con un nombre específico para poder usarla después
+
+// una variable se declara con la palabra reservada "let" y luego el nombre que le quieras asignar
+
+// Una variable puede contener cualquier valor, y como su nombre indiac, varía, pudiendo cambiar su valor en otro momento 
+
 let variable = 5;
 
 console.log (variable);
---
+++ 03-Control-Flujo/07-ForOf.js
@@ -0,0 +1,11 @@
+// El bucle for...of sirve para recorrer los valores de elementos iterables de una manera simple.
+
+// A diferencia de otros bucles, este te da directamente el valor de cada elemento en lugar de su índice o posición.
+
+let nombres = [ 'matew', 'erick', 'dylan']
+
+for ( let nombre of nombres ) {
+
+    console.log ( nombre )
+
+}
